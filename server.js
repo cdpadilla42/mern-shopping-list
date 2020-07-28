713b9694ef22661ee13112448c56bfc2aeded5bd
Chris Padilla <cdpadilla42@gmail.com>
2020-07-27 20:07:07 -0500

    Hook up back end to front end

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const items = require('./routes/api/items');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // DB Config
 
